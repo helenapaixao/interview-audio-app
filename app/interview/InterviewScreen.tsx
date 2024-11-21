10419a861d36dd8c54aa7cc8ddaf44fbe9994e25
helenapaixao <hp.helenapaixao@gmail.com>
2024-11-21 00:16:04 -0300

    chore: Remove Countdown component from InterviewScreen

--- app/interview/InterviewScreen.tsx
+++ app/interview/InterviewScreen.tsx
@@ -1,22 +1,13 @@
 "use client";
 
 import Question from "./components/Question";
-import Countdown from "./components/Countdown";
 import AudioRecorder from "./components/AudioRecorder";
 
 export default function InterviewScreen() {
-  const handleComplete = () => {
-    console.log("O tempo acabou!");
-  };
+  
   return (
     <div className="flex flex-col items-center">
       <Question />
-      <Countdown 
-       duration={30}
-       colors={["#34D399", "#FBBF24", "#EF4444"]} 
-       colorsTime={[10, 5, 0]} 
-       onComplete={handleComplete} 
-      />
       <AudioRecorder />
     </div>
   );
