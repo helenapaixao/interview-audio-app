52375f1390fbbfd8cabc5101b2a65d19adb84b1c
helenapaixao <hp.helenapaixao@gmail.com>
2024-11-21 01:06:32 -0300

    chore: Remove AudioRecorder component from InterviewScreen

--- app/interview/InterviewScreen.tsx
+++ app/interview/InterviewScreen.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import Question from "./components/Question";
-import AudioRecorder from "./components/AudioRecorder";
 
 export default function InterviewScreen() {
-  
   return (
     <div className="flex flex-col items-center">
       <Question />
-      <AudioRecorder />
     </div>
   );
 }
